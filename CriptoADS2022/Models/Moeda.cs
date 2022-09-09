7a1ce5a41cc4adde9c355b6f9dd5d096d5f549ac
CViniciusGuedes <c.vinicius.gr@gmail.com>
2022-09-08 22:45:42 -0300

    Add project files.

---
+++ CriptoADS2022/Models/Moeda.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CriptoADS2022.Models
+{
+    [Table("Moedas")]
+    public class Moeda
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name ="ID: ")]
+        public int id { get; set; }
+
+        [StringLength(35)]
+        [Required(ErrorMessage = "Campo descrição obrigatório")]
+        [Display(Name ="Descrição")]
+        public string descricao { get; set; }
+
+        [Display(Name ="Compra")]
+        [DisplayFormat(DataFormatString ="{0:C2}")]
+        public float compra { get; set; }
+
+        [Display(Name = "Venda")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public int venda { get; set; }
+    }
+}
